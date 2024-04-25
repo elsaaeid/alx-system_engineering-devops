cf64a284d66eddd6fb9fafb32a8c0f7dfaadbc3d
elsaaeid <saidsadaoy@gmail.com>
2024-04-24 19:27:20 -0700

    deploy changes

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-
 import json
 import requests
-import sys
 
-# Get the user ID from the command line arguments
-user_id = sys.argv[1]
-
-# Send a GET request to the JSONPlaceholder API
-response = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
-
-# Parse the JSON response
-tasks = response.json()
-
-# Get the username
-username = tasks[0]['username']
-
-# Prepare the data for the JSON file
-data = {user_id: [{"task": task['title'], "completed": task['completed'], "username": username} for task in tasks]}
-
-# Write the data to a JSON file
-with open(f'{user_id}.json', 'w') as jsonfile:
-    json.dump(data, jsonfile)
-
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/users"
+    users = requests.get(url, verify=False).json()
+    undoc = {}
+    udoc = {}
+    for user in users:
+        uid = user.get("id")
+        udoc[uid] = []
+        undoc[uid] = user.get("username")
+    url = "https://jsonplaceholder.typicode.com/todos"
+    todos = requests.get(url, verify=False).json()
+    [udoc.get(todo.get("userId")).append({"task": todo.get("title"),
+                                       "completed": todo.get("completed"),
+                                       username": undoc.get(
+                                               todo.get("userId"))})
+     for todo in todos]
+    with open("todo_all_employees.json", 'w') as jsf:
+        json.dump(udoc, jsf)
