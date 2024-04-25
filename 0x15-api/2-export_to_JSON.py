50958488a2362bd782a2f9f7fe158dc15416a37d
elsaaeid <saidsadaoy@gmail.com>
2024-04-24 18:32:08 -0700

    deploy changes

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
+'''
+A script to export data in the JSON format.
+'''
+
 import json
 import requests
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
-
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": todo.get("title"),
-                "completed": todo.get("completed"),
-                "username": username
-            } for todo in todos]}, jsonfile)
+if __name__ == '__main__':
+    uid = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
+    user = requests.get(url, verify=False).json()
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(uid)
+    todo = requests.get(url, verify=False).json()
+    name = user.get('username')
+    todo = [{"task": todo.get("title"),
+          "username": name,
+          "completed": todo.get("completed")} for todo in todos]
+    content = {}
+    content[uid] = todo
+    with open("{}.json".format(uid), 'w') as filejs:
+        json.dump(content, filejs)
