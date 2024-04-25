29dbf90dd7d75abf1e14a907d5a61e6253ca72ed
elsaaeid <saidsadaoy@gmail.com>
2024-04-24 17:41:18 -0700

    deploy changes

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,25 +1,18 @@
- #!/usr/bin/python3
-
+#!/usr/bin/python3
 import json
 import requests
 import sys
 
-# Get the user ID from the command line arguments
-user_id = sys.argv[1]
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-# Send a GET request to the JSONPlaceholder API
-response = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
-
-# Parse the JSON response
-tasks = response.json()
-
-# Get the username
-username = tasks[0]['username']
-
-# Prepare the data for the JSON file
-data = {user_id: [{"task": task['title'], "completed": task['completed'], "username": username} for task in tasks]}
-
-# Write the data to a JSON file
-with open(f'{user_id}.json', 'w') as jsonfile:
-    json.dump(data, jsonfile)
-
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
