032bab437ff6eb70a5388b0f26f007f6f83b332e
elsaaeid <saidsadaoy@gmail.com>
2024-04-24 19:37:21 -0700

    deploy changes

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
+"""Exports to-do list information of all employees to JSON format."""
 import json
 import requests
 
-if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com/users"
-    users = requests.get(url, verify=False).json()
-    undoc = {}
-    udoc = {}
-    for user in users:
-        uid = user.get("id")
-        udoc[uid] = []
-        undoc[uid] = user.get("username")
-    url = "https://jsonplaceholder.typicode.com/todos"
-    todos = requests.get(url, verify=False).json()
-    [udoc.get(todo.get("userId")).append({"task": todo.get("title"),
-                                       "completed": todo.get("completed"),
-                                       username": undoc.get(
-                                               todo.get("userId"))})
-     for todo in todos]
-    with open("todo_all_employees.json", 'w') as jsf:
-        json.dump(udoc, jsf)
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            user.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": user.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": user.get("id")}).json()]
+            for user in users}, jsonfile)
