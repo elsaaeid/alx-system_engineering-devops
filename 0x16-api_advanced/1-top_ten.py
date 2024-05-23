d81c001c5717ca0d8b723a2660127c09bdd15a88
elsaaeid <saidsadaoy@gmail.com>
2024-05-23 14:02:50 -0700

    deploy changes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
+Queries the Reddit API and prints the titles
+of the first 10 hot posts for a given subreddit.
 If the subreddit is invalid, it prints None.
 """
 
@@ -29,3 +30,4 @@ if __name__ == '__main__':
     else:
         subreddit = sys.argv[1]
         top_ten(subreddit)
+    
