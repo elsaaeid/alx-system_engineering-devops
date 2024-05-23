b53ae5cae93592295d49ec91c3d6dfca36fe64f1
elsaaeid <saidsadaoy@gmail.com>
2024-05-23 12:38:55 -0700

    deploy changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,8 +13,8 @@ def number_of_subscribers(subreddit):
     """
     if subreddit is None or type(subreddit) is not str:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+    res = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
                      headers={'User-Agent': '0x16-api_advanced:project:\
 v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    subs_number = res.get("data", {}).get("subscribers", 0)
+    return subs_number
